7612f982d1d668877380b41e75847e2279b242b7
Francis Vianney Salvamante <salvamante2012@gmail.com>
2024-04-12 17:11:59 +0800

    Added a blank page just to see caching in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MemesComponent } from './memes/memes.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MemesComponent],
+  imports: [RouterOutlet, RouterLink, MemesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BlankComponent } from './blank/blank.component';
+import { MemesComponent } from './memes/memes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MemesComponent,
+  },
+  {
+    path: 'blank',
+    component: BlankComponent,
+  },
+];
